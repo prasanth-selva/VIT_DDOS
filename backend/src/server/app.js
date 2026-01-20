861d73d1bddd035903ba54fbcfcdbfd451a0cd2b
yuvaraj2007 <yuvanjeyanthi@gmail.com>
2026-01-20 12:03:05 +0530

    server-yuva

---
+++ backend/src/server/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const cors = require('cors');
+const metricsRoutes = require('../metrics/routes');
+const dashboardRoutes = require('../dashboard-api/routes');
+
+function createApp({ staticDir, dashboardStatic }) {
+  const app = express();
+
+  app.use(helmet({
+    contentSecurityPolicy: false
+  }));
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cors());
+  app.use(morgan('combined'));
+
+  app.use('/metrics', metricsRoutes);
+  app.use(dashboardRoutes);
+  app.get('/health', (req, res) => res.json({ status: 'ok' }));
+
+  if (dashboardStatic) {
+    app.use(express.static(staticDir));
+    app.get('/', (req, res) => res.sendFile(`${staticDir}/index.html`));
+  }
+
+  return app;
+}
+
+module.exports = createApp;
